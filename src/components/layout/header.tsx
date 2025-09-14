ea0455762245dc1ad46b8533012071490f665768
Shakti Singh <ratansingh111786a@gmail.com>
2025-09-14 05:55:55 +0000

    add pading in lefr and right (_for element div_)

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -10,7 +10,7 @@ type HeaderProps = {
 export function Header({user}: HeaderProps) {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 items-center">
+      <div className="container flex h-16 items-center px-4 md:px-6">
         <div className="mr-4 hidden md:flex">
           <Link href="/" className="mr-6 flex items-center space-x-2">
             <Logo />
